945ef7517fce84b5870b4e418a9394f851601742
azlinaaylin <azlinaarabi@gmail.com>
2025-09-24 14:19:38 +0600

    basic setup done.Alhamdulliah

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import RootLayout from "./root_layout/RootLayout";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RootLayout />}>
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      
+    </div>
+  );
+};
+
+export default App;
